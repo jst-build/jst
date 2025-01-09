37d894941654227554686abe307ecac2f3b45522
Sascha Roloff <sascha.roloff@huawei.com>
2025-01-09 18:05:10 +0100

    progress: Call the reporter a final time in the base progress reporter

--- src/buildtool/progress_reporting/base_progress_reporter.cpp
+++ src/buildtool/progress_reporting/base_progress_reporter.cpp
@@ -23,6 +23,19 @@
 #include "src/buildtool/logging/log_level.hpp"
 #include "src/buildtool/logging/logger.hpp"
 
+namespace {
+void CallReport(std::function<void(void)> const& report) noexcept {
+    try {
+        report();
+    } catch (std::exception const& ex) {
+        Logger::Log(LogLevel::Warning,
+                    "calling progress report function failed with:\n{}",
+                    ex.what());
+        // continue with progress reporting
+    }
+}
+}  // namespace
+
 auto BaseProgressReporter::Reporter(std::function<void(void)> report,
                                     std::int64_t start_delay,
                                     double backoff_factor) noexcept
@@ -35,18 +48,12 @@ auto BaseProgressReporter::Reporter(std::function<void(void)> report,
         while (not *done) {
             cv->wait_for(lock, std::chrono::milliseconds(delay));
             if (not *done) {
-                try {
-                    report();
-                } catch (std::exception const& ex) {
-                    Logger::Log(
-                        LogLevel::Warning,
-                        "calling progress report function failed with:\n{}",
-                        ex.what());
-                    // continue with progress reporting
-                }
+                CallReport(report);
             }
             delay = static_cast<std::int64_t>(static_cast<double>(delay) *
                                               backoff_factor);
         }
+        // Call the reporter a final time to print the latest state.
+        CallReport(report);
     };
 }
